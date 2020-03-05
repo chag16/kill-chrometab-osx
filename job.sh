0bcaa116513ce94607414ff860b0d3d6a16c1733
chag16 <61829342+chag16@users.noreply.github.com>
2020-03-05 20:36:25 +0530

    Update job.sh

--- job.sh
+++ job.sh
@@ -3,11 +3,22 @@
 # Memory Threshold (% of Memory). Allows to tweak the threshold above which chrome tabs will be killed
 MT=2
 
+# Echo process IDs above Memory Threshold
+echo "Process IDs of Process above Memory Threshold: $MT%"
+ps -awxm -o %mem,cpu,pid,rss,vsz,comm | sort -nr | grep 'Google Chrome Helper' | awk -v mt=$MT '{if ($1 > mt ) print $3}'
+
+
 #Method 1: To test on run time, works without checks
+echo "Killing these PIDs"
 kill -9 $(ps -awxm -o %mem,cpu,pid,rss,vsz,comm | sort -nr | grep 'Google Chrome Helper' | awk -v mt=$MT '{if ($1 > mt ) print $3}')
+echo "Done (error means no process to kill)"
 
 #Method 2: Works only when not on AC power
-#if ! pmset -g batt | grep 'AC Power'
-#then
-#kill -9 $(ps -awxm -o %mem,cpu,pid,rss,vsz,comm | sort -nr | grep 'Google Chrome Helper' | awk -v mt=$MT '{if ($1 > mt ) print $3}')
-#fi
+: '
+echo "Killing these PIDs"
+if ! pmset -g batt | grep "AC Power"
+then
+kill -9 $(ps -awxm -o %mem,cpu,pid,rss,vsz,comm | sort -nr | grep "Google Chrome Helper" | awk -v mt=$MT "{if ($1 > mt ) print $3}")
+fi
+echo "Done (error means no process to kill)"
+'
