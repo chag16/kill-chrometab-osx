82d0f8971034f4906b617dc5af672dd055672988
chag16 <61829342+chag16@users.noreply.github.com>
2020-03-05 20:02:27 +0530

    Added ability to tweak thresh hold

--- job.sh
+++ job.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-kill -9 $(ps -awxm -o %mem,cpu,pid,rss,vsz,comm | sort -nr | grep 'Google Chrome Helper' | awk '{if ($1 > 2 ) print $3}')
+# Memory Threshold (% of Memory). Allows to tweak the threshold above which chrome tabs will be killed
+MT=2
+
+kill -9 $(ps -awxm -o %mem,cpu,pid,rss,vsz,comm | sort -nr | grep 'Google Chrome Helper' | awk -v mt=$MT '{if ($1 > mt ) print $3}')
