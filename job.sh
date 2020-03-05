2af8fc50babeaa6bb458a3abaa7b3813ae902ab3
chag16 <61829342+chag16@users.noreply.github.com>
2020-03-05 20:39:42 +0530

    Update job.sh

--- job.sh
+++ job.sh
@@ -4,21 +4,21 @@
 MT=2
 
 # Echo process IDs above Memory Threshold
-echo "Process IDs of Process above Memory Threshold: $MT%"
+echo "Process IDs of process above Memory Threshold: $MT%"
 ps -awxm -o %mem,cpu,pid,rss,vsz,comm | sort -nr | grep 'Google Chrome Helper' | awk -v mt=$MT '{if ($1 > mt ) print $3}'
 
 
 #Method 1: To test on run time, works without checks
-echo "Killing these PIDs"
+echo "Killing these PIDs (error means none)"
 kill -9 $(ps -awxm -o %mem,cpu,pid,rss,vsz,comm | sort -nr | grep 'Google Chrome Helper' | awk -v mt=$MT '{if ($1 > mt ) print $3}')
-echo "Done (error means no process to kill)"
+echo "Killed"
 
 #Method 2: Works only when not on AC power
 : '
-echo "Killing these PIDs"
+echo "Killing these PIDs (error means none)"
 if ! pmset -g batt | grep "AC Power"
 then
 kill -9 $(ps -awxm -o %mem,cpu,pid,rss,vsz,comm | sort -nr | grep "Google Chrome Helper" | awk -v mt=$MT "{if ($1 > mt ) print $3}")
 fi
-echo "Done (error means no process to kill)"
+echo "Killed"
 '
